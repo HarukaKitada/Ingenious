ba4bf0272a1356c4f01deffdac75182d74db9227
北田遥香 <kitadaharuka@kitadaharukanoMacBook-Pro.local>
2020-10-20 12:49:07 +0900

    main-img

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-var loop = setInterval(function() {
-  //li先頭要素のクローンを作成
-  var clone = $(".slide li:first").clone(true);
-  //li先頭要素のマージントップにマイナスを指定しアニメーションさせる
-  $(".slide li:first").animate({
-  marginTop : "0px"
-  }, {
-    //切り替えのスピード
-  duration : 1500,
-  complete : function() {
-      //処理完了時に先頭要素を削除
-      $(".slide li:first").remove();
-      //クローンをliの最後に追加
-      clone.clone(true).insertAfter($(".slide li:last"));
-  }
-  });
-}, 1500);
+$(window).load(function() {
+	var img = $("#slideshow").children("img"), // 画像を取得
+		num = img.length, // 画像の数を数える
+		count = 0, // 現在何枚目を表示しているかのカウンター
+		interval = 3500; // 次の画像に切り替わるまでの時間(1/1000秒)
+	
+	img.eq(0).addClass("show"); // 最初の画像にshowクラス付与
+	
+	setTimeout(slide, interval); // slide関数をタイマーセット
+	
+	function slide() {
+		img.eq(count).removeClass("show"); // 現在表示している画像からshowクラスを取り除く
+		count++; // カウンターを一個進める
+		if(count >= num) {
+			count = 0; // カウンターが画像の数より大きければリセット
+		}
+		img.eq(count).addClass("show"); // 次の画像にshowクラス付与
+		setTimeout(slide, interval); // 再びタイマーセット
+	}
+});
